c3ba8631943235bae8f2e204eba9882be5b1b148
Valentin RONCORONI <valentin.ronco@live.fr>
2026-04-02 11:22:04 +0200

    [FIX] - encrypt refactor

--- scripts/fileencrypt.sh
+++ scripts/fileencrypt.sh
@@ -1,18 +1,41 @@
 #!/bin/sh
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 <RECIPIENT:EMAIL_REMOTE>"
-  exit 1
+
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+if [ $# -lt 2 ]; then
+    log "Usage: $0 <PATH_TO_FILE> <RECIPIENT:EMAIL_REMOTE>"
+    exit 1
 fi
 
-RECIPIENT="$1"
-
-for FILE in /app/depot/*.txt; do
-  if [ ! -e "$FILE" ]; then
-    echo "Erreur: Aucun fichier trouvé correspondant à '$FILE'."
+FILE="$1"
+RECIPIENT="$2"
+if [ ! -e "$FILE" ]; then
+    log "Error: No file found at '$FILE'."
     exit 1
-  fi
-  sudo -u pgpuser gpg --batch --yes --encrypt --armor --recipient "$RECIPIENT" $FILE
-  sudo -u pgpuser rm $FILE -f
-done
+fi
 
+# Extract the base name and extension
+BASE_NAME="${FILE%.*}"
+EXTENSION="${FILE##*.}"
+ENCRYPTED_FILE="${BASE_NAME}_ENC.${EXTENSION}"
 
+log "Starting encryption process for file: $FILE with recipient: $RECIPIENT..."
+
+log "Encrypting file: $FILE"
+sudo -u pgpuser gpg --batch --yes --encrypt --armor --recipient "$RECIPIENT" "$FILE" > "$ENCRYPTED_FILE"
+if [ $? -ne 0 ]; then
+    log "Error: Failed to encrypt file '$FILE'."
+    exit 1
+fi
+
+log "Removing original file: $FILE"
+sudo -u pgpuser rm "$FILE" -f
+if [ $? -ne 0 ]; then
+    log "Error: Failed to remove file '$FILE'."
+    exit 1
+fi
+
+log "File '$FILE' encrypted and original file removed successfully."
+log "Encryption process completed."
